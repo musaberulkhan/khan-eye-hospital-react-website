edd258f5c7eda045f5e9bd257addfd6539112f2a
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-19 02:43:00 +0600

    More Services Added and Service Displaying

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -16,22 +16,13 @@ const Services = () => {
     return (
         <div className="container">
             <h1 className="text-center my-5">Services</h1>
-            <div class="row row-cols-1 row-cols-md-3 g-4">                
+            <div className="row row-cols-1 row-cols-md-4 g-4">                
                     {
                         services.map(service => <Service
                             key={service.id}
+                            service={service}
                         ></Service>)
-                    }
-                    {/* <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class ="card-body">
-                        <h5 class ="card-title">Card title</h5>
-                        <p class ="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
-                        </div>
-                        <div class ="card-footer">
-                        <small class ="text-muted">Last updated 3 mins ago</small>
-                        </div>
-                    </div> */}
+                    }                    
                 </div>
             </div>
 
